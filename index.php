e1caf10e0966629f549d1465902ba9b86942ad69
Alex <30621671+Ciriko@users.noreply.github.com>
2018-01-08 08:15:13 +0000

    Add basic structure.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Ciriko</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="site">
+      <h1>Welcome to my website.</h1>
+    </div>
+  </body>
+</html>
